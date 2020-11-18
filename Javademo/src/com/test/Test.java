7acaa384668d5a4744a851705390e6d77ee4ef65
admin <1485373120@qq.com>
2020-11-18 13:15:38 +0800

    这是我第一次提交

--- Javademo/src/com/test/Test.java
+++ Javademo/src/com/test/Test.java
@@ -3,7 +3,8 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello,world!!");
+		//打印一个hello world!!!
+		System.out.println("hello world!!");
 
 	}
 
